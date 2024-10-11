f9982ab6e6eabfbda61940d5b9bf6bc9764e253c
ORosales <oscar0931996@gmail.com>
2024-10-11 03:12:00 -0600

    2.11 PhoneBook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Person from './components/Person'
 
 const Filter=(props)=>{
@@ -28,13 +29,20 @@ const Persons=({filteredPersons})=>{
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFiltered, setNewFiltered] = useState('')
   const [filteredPersons, setFilteredPersons] = useState(persons) // Nuevo estado para personas filtradas
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+        setFilteredPersons(response.data)
+      })
+  }, [])
 
   const addPerson = (event) => {
     event.preventDefault()
