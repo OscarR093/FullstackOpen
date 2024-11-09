eb2a31e8e07256faaadd9b9da784afba0523c410
ORosales <oscar0931996@gmail.com>
2024-11-09 13:12:09 -0600

    3.12 phonebook_backend terminal database

--- part3/phonebook_backend/mongo.js
+++ part3/phonebook_backend/mongo.js
@@ -1,24 +1,32 @@
 const mongoose = require('mongoose')
 
+//comprobar contraseña
 if (process.argv.length<3 ) {
   console.log('wrong arguments, needed password')
   process.exit(1)
 }
 
+//datos estaticos
 const password = process.argv[2]
 const url = `mongodb+srv://fullStack:${password}@fullstackcluster.yk7vf.mongodb.net/phoneBookDB?retryWrites=true&w=majority&appName=fullstackCluster`
 const name=process.argv[3]
 const number=process.argv[4]
 
-
-const addPerson=(name, number, url)=>{
-mongoose.set('strictQuery',false)
-mongoose.connect(url)
-
-const noteSchema = new mongoose.Schema({
-  name: String,
+const schema={ 
+  name: String, 
   number: String,
-})
+}
+
+//establecer conexion con mongoDB 
+const setConection=(url)=>{
+  mongoose.set('strictQuery',false)
+  mongoose.connect(url)
+}
+
+//metodo agregar persona
+const addPerson=(name, number, url)=>{
+setConection(url)
+const noteSchema = new mongoose.Schema(schema)
 const Person = mongoose.model('persons', noteSchema)
 const person = new Person({
     name:name,
@@ -30,27 +38,23 @@ person.save().then(result => {
   })
 }
 
+//mostrar personas
 const showPersons=(url)=>{
-    const noteSchema = new mongoose.Schema({
-        name: String,
-        number: String,
-      })
-      const Person = mongoose.model('persons', noteSchema)
-
-    mongoose.set('strictQuery',false)
-    mongoose.connect(url)
-    Person.find({}).then(result => {
-        result.forEach(note => {
-        console.log(note)
-        })
-        mongoose.connection.close()
-      })}
+  setConection(url)
+  const noteSchema = new mongoose.Schema(schema)
+  const Person = mongoose.model('persons', noteSchema)
+  Person.find({}).then(result => {
+    result.forEach(note => {
+    console.log(note)
+    })
+  mongoose.connection.close()
+  })}
     
-      if (process.argv.length===5) {
-        addPerson(name,number,url)
+if (process.argv.length===5) {
+  addPerson(name,number,url)
+}
+else{
+    if(process.argv.length===3){
+      showPersons(url)
       }
-      else{
-        if(process.argv.length===3){
-            showPersons(url)
-        }
-      }
+    }
